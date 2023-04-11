57ccc23be4d97197164453f6b24334f0842d8b64
Oskar <Oskar@DESKTOP-HK7VK3E>
2023-04-11 19:46:43 +0200

    Primer commit de parte de Oskar suuuu.

---
+++ paginaWeb/src/test/java/com/web/skins/PaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.skins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
