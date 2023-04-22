c2919552f69ac274d57def020e12ec71044a6058
Oskar <Oskar@DESKTOP-HK7VK3E>
2023-04-22 20:40:20 +0200

    Se añaden cositas - Oskar.

--- paginaWeb/src/main/java/com/web/skins/controller/skinsController.java
+++ paginaWeb/src/main/java/com/web/skins/controller/skinsController.java
@@ -1,8 +1,12 @@
 package com.web.skins.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class skinsController {
-
+	@GetMapping(value = "/")
+	public String inicio() {
+		return "home";
+	}
 }
